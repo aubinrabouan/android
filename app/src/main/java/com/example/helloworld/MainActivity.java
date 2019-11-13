3cd410ab205487a6470b570ecaf1e9b09a1c038c
aubinrabouan <aubinrabouan@gmail.com>
2019-11-13 23:22:15 +0100

    first working app

--- app/src/main/java/com/example/helloworld/MainActivity.java
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -3,12 +3,26 @@ package com.example.helloworld;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private int mCount;
+    TextView mDisplay;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mDisplay = findViewById(R.id.mDisplay);
+        mCount=0;
+        mDisplay.setText(String.format("%d",mDisplay));
+    }
+
+
+    public void testonclick(View view) {
+        mCount++;
+        if ( mDisplay != null) mDisplay.setText(String.format("%d",mDisplay));
     }
 }
